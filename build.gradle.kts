230ef1cadd249a37e13a26797e9e702d216d201f
Kammerlo <thomas.kammerlocher@cardanofoundation.org>
2025-03-31 08:59:29 +0200

    chore: upgrade sonar

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     id("com.diffplug.spotless") version "6.19.0"
     id("maven-publish")
     id("jacoco")
-    id("org.sonarqube") version "4.3.0.3225"
+    id("org.sonarqube") version "6.1.0.5360"
     id("org.cyclonedx.bom") version "2.2.0"
 }
 
