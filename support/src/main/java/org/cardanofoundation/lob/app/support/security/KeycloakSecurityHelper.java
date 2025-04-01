1b1f4dcf902aee352671e9b48f9168a586f3deb1
Kammerlo <thomas.kammerlocher@cardanofoundation.org>
2025-04-01 14:55:48 +0200

    feat: adding custom content filtering to enable the organization check

--- support/src/main/java/org/cardanofoundation/lob/app/support/security/KeycloakSecurityHelper.java
+++ support/src/main/java/org/cardanofoundation/lob/app/support/security/KeycloakSecurityHelper.java
@@ -6,10 +6,10 @@ import java.util.List;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 
-@Service
+@Component
 public class KeycloakSecurityHelper {
 
     public boolean canUserAccessOrg(String orgId) {
--- support/src/main/java/org/cardanofoundation/lob/app/support/spring_web/BaseRequest.java
+++ support/src/main/java/org/cardanofoundation/lob/app/support/spring_web/BaseRequest.java
@@ -7,13 +7,15 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public abstract class BaseRequest {
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class BaseRequest {
 
     @Schema(example = "75f95560c1d883ee7628993da5adf725a5d97a13929fd4f477be0faf5020ca94")
     @NotBlank(message = "Organisation Id is mandatory and must not be blank or null.")
