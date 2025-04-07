e7a85b11e072fb8aff0b20694845e3b770535e01
Kammerlo <thomas.kammerlocher@cardanofoundation.org>
2025-04-07 10:38:49 +0200

    chore: skipping security check if keycloak is disabled

--- support/src/main/java/org/cardanofoundation/lob/app/support/security/KeycloakSecurityHelper.java
+++ support/src/main/java/org/cardanofoundation/lob/app/support/security/KeycloakSecurityHelper.java
@@ -3,6 +3,7 @@ package org.cardanofoundation.lob.app.support.security;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -12,8 +13,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class KeycloakSecurityHelper {
 
+    @Value("${keycloak.enabled:true}")
+    private boolean keycloakEnabled;
+
     public boolean canUserAccessOrg(String orgId) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        // skipping if keycloak is not enabled
+        if (!keycloakEnabled) {
+            return true;
+        }
 
         if (authentication != null && authentication.getPrincipal() instanceof Jwt jwt) {
             List<String> organisations = jwt.getClaimAsStringList("organisations");
