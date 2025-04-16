e45a8783f7952a6cc90b34fcb3baf238e3c893b3
Kammerlo <thomas.kammerlocher@cardanofoundation.org>
2025-04-16 08:00:22 +0200

    chore: remove unused gradle dependencies

--- netsuite_altavia_erp_adapter/build.gradle.kts
+++ netsuite_altavia_erp_adapter/build.gradle.kts
@@ -3,11 +3,8 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
     implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
-    implementation("org.apache.httpcomponents.client5:httpclient5")
 
     implementation(project(":accounting_reporting_core"))
     implementation(project(":organisation"))
     implementation(project(":support"))
-
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
 }
