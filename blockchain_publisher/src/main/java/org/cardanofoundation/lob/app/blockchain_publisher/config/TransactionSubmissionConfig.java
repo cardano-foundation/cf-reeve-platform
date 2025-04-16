ccca1f08a726d45c6b5a3259cd0ff374738b0bd7
Thomas Kammerlocher <thomas.kammerlocher@cardanofoundation.org>
2025-04-16 12:33:07 +0200

    Fix/lob 1148 api publisher production profile fail to run migrations (#220)
    * fix: adjusting configs for prod profile
    
    * chore: adjusting submit profiles

--- blockchain_publisher/src/main/java/org/cardanofoundation/lob/app/blockchain_publisher/config/TransactionSubmissionConfig.java
+++ blockchain_publisher/src/main/java/org/cardanofoundation/lob/app/blockchain_publisher/config/TransactionSubmissionConfig.java
@@ -26,14 +26,14 @@ import org.cardanofoundation.lob.app.blockchain_reader.BlockchainReaderPublicApi
 public class TransactionSubmissionConfig {
 
     @Bean
-    @Profile(value = { "dev--yaci-dev-kit", "test"} )
+    @Profile(value = { "blockfrost"} )
     public BlockchainTransactionSubmissionService backendServiceTransactionSubmissionService(
             @Qualifier("yaci_blockfrost") BackendService backendService) {
         return new BackendServiceBlockchainTransactionSubmissionService(backendService);
     }
 
     @Bean
-    @Profile(value = { "dev--yaci-dev-kit", "test"} )
+    @Profile(value = { "blockfrost"} )
     public UtxoSupplier utxoSupplier(@Qualifier("yaci_blockfrost") BackendService backendService) {
         return new DefaultUtxoSupplier(backendService.getUtxoService());
     }
@@ -101,12 +101,21 @@ public class TransactionSubmissionConfig {
 //    }
 
     @Bean
-    @Profile( value = { "prod", "dev--preprod" } )
+    @Profile( value = { "submit-api" } )
     public BlockchainTransactionSubmissionService cardanoSummitTransactionSubmissionService(HttpClient httpClient,
                                                                                             @Value("${lob.blockchain_publisher.tx.submit.url}") String cardanoSubmitApiUrl,
                                                                                             @Value("${lob.blockchain_publisher.tx.submit.timeout.in.seconds}") int timeoutInSeconds,
                                                                                             @Value("${lob.blockchain_publisher.tx.submit.api_key}") String apiKey) {
         return new CardanoSubmitApiBlockchainTransactionSubmissionService(cardanoSubmitApiUrl, apiKey, httpClient, timeoutInSeconds);
+    }
+
+    @Bean
+    @Profile( value = { "submit-api" } )
+    public HttpClient httpClient(@Value("${lob.blockchain_publisher.tx.submit.timeout.in.seconds:30}") int timeoutInSeconds) {
+        return HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_2)
+                .connectTimeout(java.time.Duration.ofSeconds(timeoutInSeconds))
+                .build();
     }
 
 }
