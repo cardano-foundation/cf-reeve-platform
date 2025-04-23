6c6b6715ef0bec71b8ed08b001a33a6e5491e922
Kammerlo <thomas.kammerlocher@cardanofoundation.org>
2025-04-23 11:17:05 +0200

    chore: stashing

--- accounting_reporting_core/src/main/java/org/cardanofoundation/lob/app/accounting_reporting_core/repository/TransactionBatchRepository.java
+++ accounting_reporting_core/src/main/java/org/cardanofoundation/lob/app/accounting_reporting_core/repository/TransactionBatchRepository.java
@@ -1,13 +1,72 @@
 package org.cardanofoundation.lob.app.accounting_reporting_core.repository;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Set;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import org.cardanofoundation.lob.app.accounting_reporting_core.domain.core.TransactionStatus;
+import org.cardanofoundation.lob.app.accounting_reporting_core.domain.core.TransactionType;
+import org.cardanofoundation.lob.app.accounting_reporting_core.domain.entity.RejectionReason;
 import org.cardanofoundation.lob.app.accounting_reporting_core.domain.entity.TransactionBatchEntity;
 
 public interface TransactionBatchRepository extends JpaRepository<TransactionBatchEntity, String>, CustomTransactionBatchRepository {
 
     List<TransactionBatchEntity> findAllByFilteringParametersOrganisationId(String organisationId);
+
+    @Query(
+            value = """
+                    SELECT tb FROM accounting_reporting_core.TransactionBatchEntity tb
+                    JOIN accounting_reporting_core.TransactionEntity tx
+                    JOIN accounting_reporting_core.TransactionItemEntity ti
+                    WHERE
+                    tb.filteringParameters.organisationId = :organisationId
+                    AND :fromDate = NULL OR tb.filteringParameters.from >= :fromDate
+                    AND :toDate = NULL OR tb.filteringParameters.to <= :toDate
+                    AND :transactionTypes = NULL OR tx.transactionType IN :transactionTypes
+                    AND :txStatuses = NULL OR tx.overallStatus IN :txStatuses
+                    AND :hasApprove = NULL AND :hasPending = NULL AND :hasInvalid = NULL AND :hasPublish = NULL AND :hasPublished = NULL AND :hasDispatched = NULL
+                        OR (
+                            :hasApprove = TRUE
+                            AND ti.rejection.rejectionReason NOT IN :lobRejectionReasons
+                            AND ti.rejection.rejectionReason NOT IN :erpRejectionReasons
+                        )
+                        OR (
+                            :hasPending = TRUE
+                            AND EXISTS(
+                                SELECT 1 FROM accounting_reporting_core.TransactionItemEntity ti2
+                                WHERE ti2.transaction = tx
+                                AND ti2.rejection.rejectionReason IN :lobRejectionReasons
+                            )
+                        )
+                        OR (
+                            :hasInvalid = TRUE
+                            AND EXISTS(
+                                SELECT 1 FROM accounting_reporting_core.TransactionItemEntity ti2
+                                WHERE ti2.transaction = tx
+                                AND ti2.rejection.rejectionReason IN (:erpRejectionReasons)
+                            )
+                        )
+                    GROUP BY tb.id
+                    """
+    )
+    List<TransactionBatchEntity> filterBatchEntities(@Param("organisationId") String organisationId,
+                                                     @Param("fromDate") LocalDate fromDate,
+                                                     @Param("toDate") LocalDate toDate,
+                                                     @Param("transactionTypes") Set<TransactionType> transactionTypes,
+                                                     @Param("txStatuses") Set<TransactionStatus> txStatuses,
+                                                     @Param("hasInvalid") Boolean hasInvalid,
+                                                     @Param("hasPending") Boolean hasPending,
+                                                     @Param("hasApprove") Boolean hasApprove,
+                                                     @Param("hasPublish") Boolean hasPublish,
+                                                     @Param("hasPublished") Boolean hasPublished,
+                                                     @Param("hasDispatched") Boolean hasDispatched,
+                                                     @Param("erpRejectionReasons") Set<RejectionReason> erpRejectionReasons,
+                                                     @Param("lobRejectionReasons") Set<RejectionReason> lobRejectionReasons,
+                                                     Pageable pageable);
 
 }
--- accounting_reporting_core/src/main/java/org/cardanofoundation/lob/app/accounting_reporting_core/repository/TransactionBatchRepositoryGateway.java
+++ accounting_reporting_core/src/main/java/org/cardanofoundation/lob/app/accounting_reporting_core/repository/TransactionBatchRepositoryGateway.java
@@ -1,5 +1,8 @@
 package org.cardanofoundation.lob.app.accounting_reporting_core.repository;
 
+import static org.cardanofoundation.lob.app.accounting_reporting_core.domain.entity.RejectionReason.getSourceBasedRejectionReasons;
+
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -7,12 +10,15 @@ import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import org.cardanofoundation.lob.app.accounting_reporting_core.domain.core.Source;
 import org.cardanofoundation.lob.app.accounting_reporting_core.domain.entity.TransactionBatchEntity;
 import org.cardanofoundation.lob.app.accounting_reporting_core.domain.entity.TransactionEntity;
 import org.cardanofoundation.lob.app.accounting_reporting_core.resource.requests.BatchSearchRequest;
+import org.cardanofoundation.lob.app.accounting_reporting_core.resource.requests.LedgerDispatchStatusView;
 
 @Service
 @Slf4j
@@ -33,7 +39,22 @@ public class TransactionBatchRepositoryGateway {
     }
 
     public List<TransactionBatchEntity> findByFilter(BatchSearchRequest body) {
-        return transactionBatchRepository.findByFilter(body);
+        return transactionBatchRepository.filterBatchEntities(
+                body.getOrganisationId(),
+                body.getFrom(),
+                body.getTo(),
+                body.getTransactionTypes().isEmpty() ? null : body.getTransactionTypes(),
+                body.getTxStatus().isEmpty() ? null : body.getTxStatus(),
+                body.getBatchStatistics().stream().anyMatch(ledgerDispatchStatusView -> ledgerDispatchStatusView == LedgerDispatchStatusView.INVALID),
+                body.getBatchStatistics().stream().anyMatch(ledgerDispatchStatusView -> ledgerDispatchStatusView == LedgerDispatchStatusView.PENDING),
+                body.getBatchStatistics().stream().anyMatch(ledgerDispatchStatusView -> ledgerDispatchStatusView == LedgerDispatchStatusView.APPROVE),
+                body.getBatchStatistics().stream().anyMatch(ledgerDispatchStatusView -> ledgerDispatchStatusView == LedgerDispatchStatusView.PUBLISH),
+                body.getBatchStatistics().stream().anyMatch(ledgerDispatchStatusView -> ledgerDispatchStatusView == LedgerDispatchStatusView.PUBLISHED),
+                body.getBatchStatistics().stream().anyMatch(ledgerDispatchStatusView -> ledgerDispatchStatusView == LedgerDispatchStatusView.DISPATCHED),
+                new HashSet<>(getSourceBasedRejectionReasons(Source.ERP)),
+                new HashSet<>(getSourceBasedRejectionReasons(Source.LOB)),
+                PageRequest.of(body.getPage(), body.getLimit())
+        );
     }
 
     public Long findByFilterCount(BatchSearchRequest body) {
