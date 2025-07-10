84c19f24f9e2c0711dbbd6510e6d599d04bc1c0a
Thomas Kammerlocher <thomas.kammerlocher@cardanofoundation.org>
2025-07-10 06:23:17 +0200

    fix: creating a chart of account ended up in an exception, since the definition of the POST Mapping was wrong (#338)

--- organisation/src/main/java/org/cardanofoundation/lob/app/organisation/resource/ChartOfAccountController.java
+++ organisation/src/main/java/org/cardanofoundation/lob/app/organisation/resource/ChartOfAccountController.java
@@ -84,17 +84,17 @@ public class ChartOfAccountController {
                     {@Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = ChartOfAccountView.class)))}
             ),
     })
-    @PostMapping(value = "/{orgId}/chart-of-accounts", produces = MediaType.MULTIPART_FORM_DATA_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/{orgId}/chart-of-accounts", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     @PreAuthorize("hasRole(@securityConfig.getManagerRole()) or hasRole(@securityConfig.getAccountantRole()) or hasRole(@securityConfig.getAdminRole())")
     public ResponseEntity<?> insertChartOfAccount(@PathVariable("orgId") @Parameter(example = "75f95560c1d883ee7628993da5adf725a5d97a13929fd4f477be0faf5020ca94") String orgId,
                                                   @Valid @RequestBody ChartOfAccountUpdate chartOfAccountUpdate) {
 
-        ChartOfAccountView referenceCode = chartOfAccountsService.insertChartOfAccount(orgId, chartOfAccountUpdate);
-        if(referenceCode.getError().isPresent()){
-            return ResponseEntity.status(referenceCode.getError().get().getStatus().getStatusCode()).body(referenceCode);
+        ChartOfAccountView chartOfAccountView = chartOfAccountsService.insertChartOfAccount(orgId, chartOfAccountUpdate);
+        if(chartOfAccountView.getError().isPresent()){
+            return ResponseEntity.status(chartOfAccountView.getError().get().getStatus().getStatusCode()).body(chartOfAccountView);
         }
 
-        return ResponseEntity.ok(referenceCode);
+        return ResponseEntity.ok(chartOfAccountView);
     }
 
     @Operation(description = "Chart Of Account insert by csv", responses = {
