c626451199f350f147d25e0657cbb57a5fb7e764
Thomas Kammerlocher <thomas.kammerlocher@cardanofoundation.org>
2025-08-05 14:21:35 +0200

    fix: adding more validation for opening balance (#375)

--- organisation/src/main/java/org/cardanofoundation/lob/app/organisation/util/Constants.java
+++ organisation/src/main/java/org/cardanofoundation/lob/app/organisation/util/Constants.java
@@ -2,7 +2,7 @@ package org.cardanofoundation.lob.app.organisation.util;
 
 public class Constants {
 
-    public static final String VALIDATION_ERROR = "VALIDATION_ERROR";
+
 
     private Constants() {
         // Prevent instantiation
@@ -15,4 +15,6 @@ public class Constants {
     public static final String ACCOUNT_EVENT_ALREADY_EXISTS = "ACCOUNT_EVENT_ALREADY_EXISTS";
     public static final String UNABLE_TO_FIND_REFERENCE_CODE_BY_ID_S_AND_S_S = "Unable to find reference code by Id: %s and %s:%s";
     public static final String ACCOUNT_EVENT_NOT_FOUND = "ACCOUNT_EVENT_NOT_FOUND";
+    public static final String VALIDATION_ERROR = "VALIDATION_ERROR";
+    public static final String OPENING_BALANCE_VALIDATION_ERORR = "OPENING_BALANCE_VALIDATION_ERROR";
 }
