c9989f2c0b231f37af8e4b4a919fdbbe43cd3dcb
Kammerlo <thomas.kammerlocher@cardanofoundation.org>
2025-08-08 13:23:07 +0200

    feat: implemented filtering on /batches/{id} endpoint

---
+++ accounting_reporting_core/src/main/java/org/cardanofoundation/lob/app/accounting_reporting_core/resource/requests/BatchFilterRequest.java
@@ -0,0 +1,34 @@
+package org.cardanofoundation.lob.app.accounting_reporting_core.resource.requests;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import org.cardanofoundation.lob.app.accounting_reporting_core.domain.core.TransactionType;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BatchFilterRequest {
+
+    private List<TransactionType> transactionTypes;
+    private List<String> documentNumbers;
+    private List<String> currencyCustomerCodes;
+    private BigDecimal minFCY;
+    private BigDecimal maxFCY;
+    private BigDecimal minLCY;
+    private BigDecimal maxLCY;
+    private List<String> vatCustomerCodes;
+    private List<String> parentCostCenterCustomerCodes;
+    private List<String> costCenterCustomerCodes;
+    private List<String> counterPartyCustomerCodes;
+    private List<String> counterPartyTypes;
+    private List<String> debitAccountCodes;
+    private List<String> creditAccountCodes;
+    private List<String> eventCodes;
+}
