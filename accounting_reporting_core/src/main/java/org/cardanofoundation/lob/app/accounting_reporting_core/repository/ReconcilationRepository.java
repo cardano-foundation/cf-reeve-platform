5dbc4c3f6ab7ae8c7b69e961a0ca197b6594662e
Kammerlo <thomas.kammerlocher@cardanofoundation.org>
2025-10-07 14:58:42 +0200

    fix: adding entryDate check to violation as well

--- accounting_reporting_core/src/main/java/org/cardanofoundation/lob/app/accounting_reporting_core/repository/ReconcilationRepository.java
+++ accounting_reporting_core/src/main/java/org/cardanofoundation/lob/app/accounting_reporting_core/repository/ReconcilationRepository.java
@@ -26,8 +26,8 @@ public interface ReconcilationRepository extends JpaRepository<ReconcilationEnti
                 LEFT JOIN accounting_reporting_core.TransactionEntity tr ON rv.transactionId = tr.id
                 WHERE (r.id = tr.lastReconcilation.id OR tr.lastReconcilation.id IS NULL) AND ((rv.rejectionCode = 'TX_NOT_IN_ERP' AND tr.ledgerDispatchApproved = true) OR (rv.rejectionCode != 'TX_NOT_IN_ERP'))
                 AND (:rejectionCodes IS NULL OR rv.rejectionCode IN :rejectionCodes)
-                AND (CAST(:startDate AS date) IS NULL OR tr.entryDate >= :startDate)
-                AND (CAST(:endDate AS date) IS NULL OR tr.entryDate <= :endDate)
+                AND (CAST(:startDate AS date) IS NULL OR tr.entryDate >= :startDate OR rv.transactionEntryDate >= :startDate)
+                AND (CAST(:endDate AS date) IS NULL OR tr.entryDate <= :endDate OR rv.transactionEntryDate <= :endDate)
                 AND (:source IS NULL OR ( :source = 'ERP' AND tr.reconcilation.source = 'OK' ) OR ( :source = 'BLOCKCHAIN' AND tr.reconcilation.sink = 'OK') )
                 AND (:transactionTypes IS NULL OR tr.transactionType IN :transactionTypes OR rv.transactionType IN :transactionTypes)
                 AND (:transactionId IS NULL OR tr.id LIKE %:transactionId% OR rv.transactionId LIKE %:transactionId%)
