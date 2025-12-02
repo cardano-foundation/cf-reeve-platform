a8b29aa18f2e48bdc73ae234efda5e8c12cbdca3
Kammerlo <thomas.kammerlocher@cardanofoundation.org>
2025-12-02 10:10:40 +0100

    chore: spotless

--- reporting/src/test/java/org/cardanofoundation/lob/app/reporting/mapper/ReportTemplateMapperTest.java
+++ reporting/src/test/java/org/cardanofoundation/lob/app/reporting/mapper/ReportTemplateMapperTest.java
@@ -1,5 +1,19 @@
 package org.cardanofoundation.lob.app.reporting.mapper;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
 import org.cardanofoundation.lob.app.organisation.repository.ChartOfAccountSubTypeRepository;
 import org.cardanofoundation.lob.app.reporting.dto.ReportTemplateDto;
 import org.cardanofoundation.lob.app.reporting.dto.ReportTemplateFieldDto;
@@ -7,18 +21,6 @@ import org.cardanofoundation.lob.app.reporting.dto.ValidationRuleDto;
 import org.cardanofoundation.lob.app.reporting.dto.ValidationRuleTermDto;
 import org.cardanofoundation.lob.app.reporting.model.entity.ReportTemplateEntity;
 import org.cardanofoundation.lob.app.reporting.model.entity.ReportTemplateValidationRuleEntity;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ReportTemplateMapperTest {
