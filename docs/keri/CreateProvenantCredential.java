f11f95fc199f493403a555d80116705fa39655d1
Kammerlo <thomas.kammerlocher@cardanofoundation.org>
2025-12-11 13:11:01 +0100

    chore: added maven central

--- docs/keri/CreateProvenantCredential.java
+++ docs/keri/CreateProvenantCredential.java
@@ -5,7 +5,8 @@
 //COMPILE_OPTIONS -source 24
 //RUNTIME_OPTIONS
 
-//REPOS snapshots=https://oss.sonatype.org/content/repositories/snapshots/
+//REPOS snapshots=https://central.sonatype.com/repository/maven-snapshots/
+//REPOS central=https://repo.maven.apache.org/maven2
 //DEPS org.cardanofoundation:signify:0.1.2-ebfb904-SNAPSHOT
 //DEPS com.bloxbean.cardano:cardano-client-lib:0.7.0-beta2
 //DEPS com.bloxbean.cardano:cardano-client-backend-blockfrost:0.7.0-beta2
