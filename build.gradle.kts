225761b0d49ca7d64c0a32c082d0e6acce149b78
Florian Schumann <florian.schumann@cardanofoundation.org>
2026-02-17 15:23:31 +0100

    chore: update spring

--- build.gradle.kts
+++ build.gradle.kts
@@ -100,10 +100,9 @@ subprojects {
         }
     }
 
-    extra["springBootVersion"] = "3.3.3"
-    extra["springCloudVersion"] = "2023.0.0"
+    extra["springBootVersion"] = "3.5.8"
+    extra["springCloudVersion"] = "2025.0.1"
     extra["jMoleculesVersion"] = "2023.1.0"
-    extra["flyway.version"] = "10.20.1"
     extra["testcontainers.version"] = "1.21.4"
 
     dependencies {
