60eefb1c971079299094b3d0c21d5a5c91ca8119
Kammerlo <thomas.kammerlocher@cardanofoundation.org>
2026-03-04 12:35:30 +0100

    chore: spotless

--- reporting/src/main/java/org/cardanofoundation/lob/app/reporting/dto/ReportResponseDto.java
+++ reporting/src/main/java/org/cardanofoundation/lob/app/reporting/dto/ReportResponseDto.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
