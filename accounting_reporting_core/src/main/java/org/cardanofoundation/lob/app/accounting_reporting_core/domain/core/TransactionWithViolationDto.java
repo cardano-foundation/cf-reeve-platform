86a5c9c151a59918038ee097d5d9a8ef677da9c0
Marco Russo <108673166+M4rc0Russ0@users.noreply.github.com>
2026-03-05 02:16:04 +0000

    fix: reconciliation nullpoint exception (#600)

--- accounting_reporting_core/src/main/java/org/cardanofoundation/lob/app/accounting_reporting_core/domain/core/TransactionWithViolationDto.java
+++ accounting_reporting_core/src/main/java/org/cardanofoundation/lob/app/accounting_reporting_core/domain/core/TransactionWithViolationDto.java
@@ -2,11 +2,13 @@ package org.cardanofoundation.lob.app.accounting_reporting_core.domain.core;
 
 import java.time.LocalDateTime;
 
+import org.springframework.lang.Nullable;
+
 import org.cardanofoundation.lob.app.accounting_reporting_core.domain.entity.TransactionEntity;
 import org.cardanofoundation.lob.app.accounting_reporting_core.domain.entity.reconcilation.ReconcilationViolation;
 
 public record TransactionWithViolationDto(
-        TransactionEntity tx,
+        @Nullable TransactionEntity tx,
         ReconcilationViolation violation,
         LocalDateTime lastReconciledDate) {
 }
