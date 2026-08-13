d53c4e7a3c2f5152d63ee4015a410281ae434b33
Florian Schumann <florian.schumann@cardanofoundation.org>
2026-08-13 10:12:35 +0200

    chore: profile local disables authentication and authorization

---
+++ support/src/main/java/org/cardanofoundation/lob/app/support/security/DevSecurityConfiguration.java
@@ -0,0 +1,25 @@
+package org.cardanofoundation.lob.app.support.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * DevSecurityConfiguration
+ */
+@Configuration
+@Profile("local")
+@Slf4j
+public class DevSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return http.build();
+    }
+    
+}
